b29277a44aba671d16d36b6b9593d2769fc1261f
Sofia Milhomem <sofialeitemilhomem@gmail.com>
2025-08-26 03:31:09 +0000

    torre, 1 bispo e rainha

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
-
-
+void mover_torre(int casas) {
+    if (casas > 0) {
+        printf("->Direita\n");
+        mover_torre(casas - 1);
+    }
+}
+void mover_bispo(int casas){
+    if (casas > 0) {
+        printf("-> Cima, direita\n");
+        mover_bispo(casas -1);
+    }
+}
+void mover_rainha (int casas){
+    if (casas > 0){
+        printf("-> Esquerda\n");
+        mover_rainha(casas -1);
+    }
+}
 int main() {
-    int movtorre = 5, movbispo = 5, movrainha = 8, movcavalo = 3;
+    int  movbispo = 5, movrainha = 8, movcavalo = 3;
 
     //bispo, para cima e direita *diagonal*
     //torre para cima ou baixo *reta*
@@ -11,35 +27,45 @@ int main() {
 
 
 
-    //torre com FOR
+    //com recursivo, torre
+    int casatorre = 5;
     printf("MOVIMENTO TORRE:\n");
-    
-    for (int t= 0; t < movtorre; t++){
+    mover_torre(casatorre);
+
+   /* //torre com FOR
+    //for (int t= 0; t < movtorre; t++){
         //começa em 0 e vai até 5, com incremento
-        printf(" -> Direita\n"); //direção torre
-        
-    }
+        //printf(" -> Direita\n"); //direção torre
+        */
 
-    // bispo com while
-    printf("MOVIMENTO BISPO:\n");
-    int b = 0;
-    while (b <movbispo) { //começa em 0 e vai até 5
-        printf("-> Cima, direita\n"); //direção do bispo
-        b++;
-    }
     
-    // rainha com do while
-    int r = 0;
+  /*  // bispo com while
+    //printf("MOVIMENTO BISPO:\n");
+    //int b = 0;
+    //while (b <movbispo) { //começa em 0 e vai até 5
+       // printf("-> Cima, direita\n"); //direção do bispo
+        //b++;
+        */
+    //com recursividade
+    int casabispo = 5;
+    printf("MOVIMENTO BISPO:\n");
+    mover_bispo(casabispo);
+  
+    
+    /*// rainha com do while
+    //int r = 0;
+    //printf("MOVIMENTO RAINHA:\n");
+    // do {
+      //  printf("-> Esquerda\n "); //direção da rainha
+       // r++;
+     //} while ( r < movrainha); //começa em 0 e vai até 8
+*/
+    int casarainha =8;
     printf("MOVIMENTO RAINHA:\n");
-     do {
-        printf("-> Esquerda\n "); //direção da rainha
-        r++;
-     } while ( r < movrainha); //começa em 0 e vai até 5
+    mover_rainha(casarainha);
 
 
-
-
-     //cavaloO com for e for
+    /* //cavaloO com for e for
      printf("MOVIMENTO CAVALO:\n");
         for(int c= 0; c < movcavalo; c++){
             // baixo, 2x
@@ -53,6 +79,11 @@ int main() {
             }
             printf("\n");
         }
+            */
+    // cavalo com break e aninhado?
+    for (int c = 0; c<3; c++){ //2 movimentos cima
+        if()
+    }
 
 /*
         // cCAvalo com while e for
@@ -73,8 +104,7 @@ int main() {
 
 
 
- return 0;
-     }
-     
-   
+
+
+
 
